a7ab9558f6370bf7ec51f0aa076aec1e4af74abb
Abey Velangadan <abey.velangadan@gmail.com>
2020-02-14 07:51:15 +0530

    Add project files.

---
+++ HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core_web_api_empty_template.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }
+        [HttpGet]
+        public string get()
+        {
+            return "Hello from Home";
+        }
+    }
+}
